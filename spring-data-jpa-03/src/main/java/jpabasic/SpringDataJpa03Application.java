18963de9b09b496380f0aee53ebbfa03f33cd72a
madvirus <madvirus@madvirus.net>
2022-06-20 21:01:19 +0900

    spring-data-jpa-03: 예제 코드

--- spring-data-jpa-03/src/main/java/jpabasic/SpringDataJpa03Application.java
+++ spring-data-jpa-03/src/main/java/jpabasic/SpringDataJpa03Application.java
@@ -8,9 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @SpringBootApplication
 public class SpringDataJpa03Application implements CommandLineRunner {
@@ -28,15 +33,43 @@ public class SpringDataJpa03Application implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         jdbcTemplate.update("truncate table user");
+        for (int i = 1 ; i <= 20 ; i++) {
+            User user = new User("email" + i + "@email.com", "이름" + i, LocalDateTime.now());
+            userRepository.save(user);
+            logger.info("saved: {}", user.getEmail());
+        }
+        List<User> users = userRepository.findByNameLikeOrderByNameAscEmailDesc("이름%");
+        logger.info("users: {}", users.size());
 
-        User user1 = new User("email1@email.com", "이름1", LocalDateTime.now());
-        userRepository.save(user1);
-        logger.info("saved: {}", user1.getEmail());
-        User user2 = new User("email2@email.com", "이름2", LocalDateTime.now());
-        userRepository.save(user2);
-        logger.info("saved: {}", user2.getEmail());
+        Sort sort1 = Sort.by(Sort.Order.asc("name"));
+        List<User> users1 = userRepository.findByNameLike("이름%", sort1);
+        logger.info("users1: {}", users1.size());
 
-        // TODO
-        // userRepository.findByNameLikeOrderByNameDesc()
+        Sort sort2 = Sort.by(Sort.Order.asc("name"), Sort.Order.desc("email"));
+        List<User> users2 = userRepository.findByNameLike("이름%", sort2);
+        logger.info("users2: {}", users2.size());
+
+        Sort sort3 = Sort.by(Sort.Order.asc("name"), Sort.Order.desc("email"));
+        Pageable pageable = PageRequest.ofSize(10).withPage(1).withSort(sort3);
+        List<User> users3 = userRepository.findByNameLike("이름%", pageable);
+        logger.info("users3: {}", users3.size());
+
+        Pageable pageable2 = PageRequest.ofSize(10).withPage(0).withSort(sort3);
+        Page<User> page = userRepository.findByEmailLike("email%", pageable2);
+        long totalElements = page.getTotalElements();
+        int totalPages = page.getTotalPages();
+        List<User> content = page.getContent();
+        int size = page.getSize();
+        int pageNumber = page.getNumber();
+        int numberOfElements = page.getNumberOfElements(); // content의 개수
+        logger.info("totalElements: {}, totalPages: {}, size: {}, pageNumber: {}, numberOfElements: {}",
+                totalElements, totalPages, size, pageNumber, numberOfElements);
+
+        List<User> recentUsers1 = userRepository.findRecentUsers(LocalDateTime.now().minusDays(1));
+        logger.info("recentUsers1: {}", recentUsers1.size());
+        List<User> recentUsers2 = userRepository.findRecentUsers(LocalDateTime.now().minusDays(1), Sort.by("name"));
+        logger.info("recentUsers2: {}", recentUsers2.size());
+        Page<User> recentUsers3 = userRepository.findRecentUsers(LocalDateTime.now().minusDays(1), PageRequest.ofSize(10).withPage(0));
+        logger.info("recentUsers3: {}", recentUsers3.getContent().size());
     }
 }
--- spring-data-jpa-03/src/main/java/jpabasic/reserve/domain/UserRepository.java
+++ spring-data-jpa-03/src/main/java/jpabasic/reserve/domain/UserRepository.java
@@ -1,7 +1,13 @@
 package jpabasic.reserve.domain;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,5 +18,24 @@ public interface UserRepository extends Repository<User, String> {
 
     void delete(User user);
 
-    List<User> findByNameLike(String keyworkd);
+    List<User> findByNameLike(String keyword);
+
+    List<User> findByNameLikeOrderByNameDesc(String keyword);
+
+    List<User> findByNameLikeOrderByNameAscEmailDesc(String keyword);
+
+    List<User> findByNameLike(String keyword, Sort sort);
+
+    List<User> findByNameLike(String keyword, Pageable pageable);
+
+    Page<User> findByEmailLike(String keyword, Pageable pageable);
+
+    @Query("select u from User u where u.createDate > :since order by u.createDate desc")
+    List<User> findRecentUsers(@Param("since") LocalDateTime since);
+
+    @Query("select u from User u where u.createDate > :since")
+    List<User> findRecentUsers(@Param("since") LocalDateTime since, Sort sort);
+
+    @Query("select u from User u where u.createDate > :since")
+    Page<User> findRecentUsers(@Param("since") LocalDateTime since, Pageable pageable);
 }
